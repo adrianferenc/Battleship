1345c8e420de0f702c708ad2fe991d173fc79297
Adrian Ferenc <68762863+adrianferenc@users.noreply.github.com>
2021-06-23 23:09:51 -0700

    ai improvements

--- js/main.js
+++ js/main.js
@@ -23,8 +23,10 @@ let player,
   startButton,
   orientButton,
   buttons,
+  found,
   placedShips,
   randomSquare,
+  max,
   turn,
   gameUpdate,
   gameUpdater,
@@ -34,9 +36,6 @@ let player,
   shipyardDisplay;
 
 /*----- cached element references -----*/
-//  const playerBoardDom = document.querySelector("player-board");
-// const aiBoard = document.querySelector("ai-board");
-// const shipyard = document.querySelector("shipyard");
 
 /*----- classes -----*/
 class Square {
@@ -91,10 +90,10 @@ class Board {
       squareObject.up = above;
       squareObject.upList = this.squares[above].upList.concat([squareId]);
       this.squares[above].down = squareId;
-      squareObject.upList.forEach((ID) => {
-        this.squares[ID].downList.push(squareId);
-      });
     }
+    squareObject.upList.forEach((ID) => {
+      this.squares[ID].downList.push(squareId);
+    });
     //Updates left, right, leftList, and rightList
     if (i % 10 === 0) {
       squareObject.leftList.push(squareId);
@@ -104,10 +103,10 @@ class Board {
       squareObject.left = left;
       squareObject.leftList = this.squares[left].leftList.concat([squareId]);
       this.squares[left].right = squareId;
-      squareObject.leftList.forEach((ID) => {
-        this.squares[ID].rightList.push(squareId);
-      });
     }
+    squareObject.leftList.forEach((ID) => {
+      this.squares[ID].rightList.push(squareId);
+    });
     return squareObject;
   }
 }
@@ -214,11 +213,17 @@ function startRound() {
   document.body.innerHTML = "";
   player = new Board("player");
   shipyard = new Shipyard();
+  orientButton.classList.remove("rotated-button");
   for (let i = 0; i < 100; i++) {
     player.createSquare(i);
   }
   placedShips = {};
   orientation = "leftRight";
+  max = {
+    id: [],
+    max: 0,
+  };
+  found = [];
   buttons.setAttribute("id", "buttons");
   buttons.appendChild(orientButton);
   buttons.appendChild(startButton);
@@ -410,13 +415,99 @@ function attackASquare(e) {
   }
 }
 
+function updateSquares(side, square) {
+  let i = unsquare(square);
+  //update left, right, up, down:
+  if (i % 10 !== 0) {
+    side.squares[sq(i - 1)].right = null;
+  }
+  if (i % 10 !== 9) {
+    side.squares[sq(i + 1)].left = null;
+  }
+  if (i >= 10) {
+    side.squares[sq(i - 10)].down = null;
+  }
+  if (i < 90) {
+    side.squares[sq(i + 10)].up = null;
+  }
+  //update lists
+  for (let j = 0; (10 + i - j) % 10 <= i % 10; j++) {
+    side.squares[sq(i - j)].rightList = side.squares[sq(i - j)].rightList.slice(
+      0,
+      side.squares[sq(i - j)].rightList.indexOf(square)
+    );
+    if ((i - j) % 10 === 0 && j > 0) {
+      break;
+    }
+  }
+
+  for (let j = 0; i % 10 <= (i + j) % 10 && i + j < 100; j++) {
+    side.squares[sq(i + j)].leftList = side.squares[sq(i + j)].leftList.slice(
+      side.squares[sq(i + j)].leftList.indexOf(square) + 1
+    );
+    if ((i + j) % 10 === 0 && j > 0) {
+      break;
+    }
+  }
+
+  for (let j = 0; i - 10 * j >= 0; j++) {
+    side.squares[sq(i - 10 * j)].downList = side.squares[
+      sq(i - 10 * j)
+    ].downList.slice(0, side.squares[sq(i - 10 * j)].downList.indexOf(square));
+    if (i - 10 * j < 10) {
+      break;
+    }
+  }
+  for (let j = 0; i + 10 * j < 100; j++) {
+    side.squares[sq(i + 10 * j)].upList = side.squares[
+      sq(i + 10 * j)
+    ].upList.slice(side.squares[sq(i + 10 * j)].upList.indexOf(square) + 1);
+  }
+}
+
+function stabInTheDark() {
+  for (let i = 0; i < 100; i++) {
+    let total =
+      Math.min(player.squares[sq(i)].leftList.length, 5) +
+      Math.min(player.squares[sq(i)].rightList.length, 5) +
+      Math.min(player.squares[sq(i)].downList.length, 5) +
+      Math.min(player.squares[sq(i)].upList.length, 5);
+    if (total > max.max) {
+      max.max = total;
+    }
+  }
+  for (let i = 0; i < 100; i++) {
+    let total =
+      Math.min(player.squares[sq(i)].leftList.length, 5) +
+      Math.min(player.squares[sq(i)].rightList.length, 5) +
+      Math.min(player.squares[sq(i)].downList.length, 5) +
+      Math.min(player.squares[sq(i)].upList.length, 5);
+    if (total === max.max) {
+      max.id.push(sq(i));
+    }
+  }
+  return max.id[Math.floor(Math.random() * max.id.length)];
+}
+
 function AIAttacks() {
   if (turn % 2 === 0) {
-    do {
-      randomSquare = Math.floor(Math.random() * 100);
-    } while (player.squares[sq(randomSquare)].attacked);
-    checkHit(player, sq(randomSquare));
+    if (found.length === 0) {
+      checkHit(player, stabInTheDark());
+    } else if (found.length === 1) {
+      let leftOfFound = player.squares[found[0]].left;
+      let rightOfFound = player.squares[found[0]].right;
+      let upOfFound = player.squares[found[0]].up;
+      let downOfFound = player.squares[found[0]].down;
+      let sorted = [
+        ["left", player.squares[leftOfFound].leftList.length],
+        ["right", player.squares[rightOfFound].rightList.length],
+        ["up", player.squares[upOfFound].upList.length],
+        ["down", player.squares[downOfFound].downList.length],
+      ].sort((x,y)=> y[1]-x[1]);
+      console.log(sorted);
+    }
     turn++;
+    max = { max: 0, id: [] };
     winner = getWinner();
     render();
   }
@@ -426,18 +517,29 @@ function checkHit(side, square) {
   side.squares[square].attacked = true;
   if (side.squares[square].occupied) {
     side.squares[square].class = "hit-square";
+    if (side.name === "player") {
+      if (found.length === 0) {
+        found[0] = square;
+      } else {
+        found[1] = square;
+      }
+    }
     for (let ship in side.ships) {
       if (side.ships[ship].position.includes(unsquare(square))) {
         side.ships[ship].health--;
         if (side.ships[ship].health === 0) {
           sinkShip(ship, side);
           side.remainingShips--;
+          if (side.name === "player") {
+            found = [];
+          }
         }
       }
     }
   } else {
     side.squares[square].class = "miss-square";
   }
+  updateSquares(side, square);
 }
 
 function sinkShip(ship, side) {
